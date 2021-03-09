7127e356da859b8b58565c1c9bc82e9ae2085ef4
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2021-03-09 21:01:36 +0530

    improved symbol and production class

--- ParserCore/ContextFreeGrammer/Symbol.cs
+++ ParserCore/ContextFreeGrammer/Symbol.cs
@@ -2,25 +2,21 @@ using System.Collections.Generic;
 
 namespace Parsers.Grammer
 {
-    public abstract class Symbol
+    public class Symbol
     {
         public string SymbolName { get; init; }
-        public Symbol(string symbolName)
+        public SymbolType Type { get; init; }
+        public Symbol(string symbolName, SymbolType symbolType = SymbolType.Terminal)
         {
             SymbolName = symbolName;
+            Type = symbolType;
         }
     }
-    /// <summary>
-    /// This is defintion for terminal symbol
-    /// </summary>
-    public class TerminalSymbol : Symbol
+    public enum SymbolType
     {
-        public TerminalSymbol(string symbolName) : base(symbolName) { }
-        public static TerminalSymbol EPSILON => new("ε");
-    }
-    public class NonTerminalSymbol : Symbol
-    {
-        public List<Production> Production { get; init; }
-        public NonTerminalSymbol(string symbolName) : base(symbolName) { }
+        Terminal,
+        NonTerminal,
+        Start,
+        Default
     }
 }
--- Sample/Program.cs
+++ Sample/Program.cs
@@ -7,28 +7,56 @@ OperatorPrecedenceParser precedence = new();
 
 ProductionTable table = new();
 
-Production production = new("A");
-production.Right = new()
+// Production production = new("A");
+
+// production.Right = new()
+// {
+//     new TerminalSymbol("id^"),
+//     new NonTerminalSymbol("B"),
+//     new TerminalSymbol("+"),
+//     TerminalSymbol.EPSILON
+// };
+
+// table.Add(production);
+
+// Production production2 = new("B");
+// production2.Right = new()
+// {
+//     new TerminalSymbol("m"),
+//     new NonTerminalSymbol("X"),
+//     new TerminalSymbol("+"),
+//     new TerminalSymbol("c")
+// };
+
+// table.Add(production2);
+
+
+var input = @"A->i
+A->B+C*A
+C->A
+B->i";
+
+foreach (var v in input.Split("\n"))
 {
-    new TerminalSymbol("id^"),
-    new NonTerminalSymbol("B"),
-    new TerminalSymbol("+"),
-    TerminalSymbol.EPSILON
-};
+    var parts = v.Split("->");
+    Production production1 = new(parts[0]);
+    foreach (var s in parts[1])
+    {
+        if (s == '\r')
+            continue;
 
-table.Add(production);
-
-Production production2 = new("A");
-
-production2.Right = new()
-{
-    new TerminalSymbol("m"),
-    new NonTerminalSymbol("X"),
-    new TerminalSymbol("+"),
-    new TerminalSymbol("c")
-};
-
-table.Add(production2);
+        production1.Right ??= new List<Symbol>();
+        if (char.IsUpper(s))
+        {
+            production1.Right.Add(new Symbol(s.ToString(), SymbolType.NonTerminal));
+        }
+        else
+        {
+            production1.Right.Add(new Symbol(s.ToString()));
+        }
+    }
+    table.Add(production1);
+}
 
 foreach (var values in table)
 {
@@ -37,4 +65,4 @@ foreach (var values in table)
         WriteLine(prod.ToString());
     }
 }
-
+//A-> return Statement;
