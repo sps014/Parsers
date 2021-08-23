bb37025f601640e11ebc68a71d9b7801bd022b47
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2021-08-23 22:10:31 +0530

    adding production

---
+++ ParserLib/src/grammar/production.rs
@@ -0,0 +1,28 @@
+use crate::grammar::symbol::Symbol;
+
+#[derive(Debug,Clone,Eq, PartialEq)]
+pub struct Production
+{
+    pub left:Symbol,
+    pub right:Vec<Symbol>
+}
+impl Production
+{
+    pub fn new(left:Symbol,right:Vec<Symbol>)->Self
+    {
+        Production{left,right}
+    }
+    pub fn from_str(left:String,right:Vec<Symbol>)->Self
+    {
+        Production{left:Symbol::non_terminal_with(left),right}
+    }
+    pub fn print(&self)
+    {
+        print!("{} ->",self.left.value);
+        for i in self.right
+        {
+            print!(" {}",i.value);
+        }
+        print!("\n");
+    }
+}
