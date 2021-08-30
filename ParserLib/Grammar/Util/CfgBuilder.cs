a8e8a0ef3b4c8211e842f0a9c335dd8451fb0f80
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2021-08-30 23:25:02 +0530

    adding grammar builder

---
+++ ParserLib/Grammar/Util/CfgBuilder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserLib.Grammar.Util;
+public class CfgBuilder
+{
+    private string[] Grammar;
+    private int pos = 0;
+    public Cfg Build(string grammar)
+    {
+        pos = 0;
+        Grammar= grammar.Split(" ").Where(x=>!string.IsNullOrWhiteSpace(x)).ToArray();
+
+        Cfg lang=new Cfg();
+        while (pos<=Grammar.Length)
+        {
+            foreach (var p in ParseRule())
+            {
+                lang.AddProduction(p);
+            }
+        }
+        return lang;
+    }
+    private List<Production> ParseRule()
+    {
+        var left = ParseRule();
+    }
+    private Symbol ParseLeft()
+    {
+        var l = GetCurrent();
+        if (l == null)
+            throw new Exception("Reached end at without finding a valid Left hand side of Grammar");
+        if((l.StartsWith('\'') && l.EndsWith('\'')) || !char.IsLetter(l[0]))
+            throw new Exception($"Expected a non terminal (starting with Alphabet) but found {l}");
+
+        return Symbol.NonTerminal(l);
+    }
+    private string GetCurrent()
+    {
+        if(pos+1<Grammar.Length)
+        {
+            return Grammar[pos++];
+        }
+        return null;
+    }
+}
+
--- Sample/Program.cs
+++ Sample/Program.cs
@@ -3,68 +3,12 @@ using System;
 using System.Collections.Generic;
 using ParserLib;
 using ParserLib.Grammar;
+using ParserLib.Grammar.Util;
 
-var nt = Symbol.NonTerminal("K");
-var p = new Production(nt, 
-    new()
-    {
-        new Symbol("a"),nt,new Symbol("b")
-    }
-);
-var p1 = new Production(nt, 
-    new()
-    {
-        new Symbol("a"),nt,new Symbol("c")
-    }
-);
-var p2 = new Production(nt, 
-    new()
-    {
-        new Symbol("b"),nt,new Symbol("c")
-    }
-);
-var p3 = new Production(nt, 
-    new()
-    {
-        new Symbol("c"),nt,new Symbol("d")
-    }
-);
-var p4 = new Production(nt, 
-    new()
-    {
-        new Symbol("c"),nt,new Symbol("e")
-    }
-);
-var cfg=new Cfg();
+var grammar = @"
+A -> 'b' '+' C '\'' | 
+ \| 'd' ''
+";
 
-cfg.AddProduction(p);
-cfg.AddProduction(p1);
-cfg.AddProduction(p2);
-cfg.AddProduction(p3);
-cfg.AddProduction(p4);
 
-cfg.EliminateLeftFactoring();
-//cfg.Print();
-
-cfg = new Cfg();
-
-p = new Production(nt,
-    new()
-    {
-        nt,
-        new Symbol("b")
-    }
-);
-p1 = new Production(nt,
-    new()
-    {
-        new Symbol("a"),
-        nt,
-        new Symbol("c")
-    }
-);
-cfg.AddProduction(p1);
-cfg.AddProduction(p);
-cfg.Print();
-cfg.EliminateLeftRecursion();
-cfg.Print();
+var cfg =CfgBuilder.Build(grammar);
