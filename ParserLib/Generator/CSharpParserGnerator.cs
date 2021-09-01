33baf7f3dc84abb31d37a8f46918f840a9ecc3f1
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2021-09-01 16:17:04 +0530

    Update CSharpParserGnerator.cs

--- ParserLib/Generator/CSharpParserGnerator.cs
+++ ParserLib/Generator/CSharpParserGnerator.cs
@@ -18,27 +18,51 @@ public class CSharpParserGnerator : ParserGenerator
         StringBuilder sb = new StringBuilder("");
         foreach(var k in Grammar.NonTerminals)
         {
-            int c = 0;
-            var epslion = new Production(k, new List<Symbol> { Symbols.EPSILON });
-
-            foreach (var p in Grammar[k])
-            {
-                if (p == epslion)
-                    continue;
-                ProcessProduction(p, sb,c==0);
-                c++;
-            }
-            if(Grammar[k].Contains(epslion))
-            {
-                //generate else part if only one production generate empty function
-            }
+            GenrateFunction(k,sb);
         }
+        Console.WriteLine(sb.ToString());
         return sb.ToString();
     }
-    private void ProcessProduction(Production p,StringBuilder sb,bool first=false)
+    
+    private void GenrateFunction(Symbol s,StringBuilder sb)
     {
-        GenerateNestedProduction(p.Right,0,sb,0,first);
+        sb.Append($"private void {FunctionName(s)}()\r\n");
+        sb.Append("{\r\n");
+
+
+        int c = 0;
+        var epslion = new Production(s, new List<Symbol> { Symbols.EPSILON });
+        var has_eps = Grammar[s].Contains(epslion);
+
+        foreach (var p in Grammar[s])
+        {
+            if (p == epslion)
+                continue;
+            ProcessProduction(p, sb, c == 0);
+            c++;
+        }
+        HandleEpsilone(has_eps, sb);
+
+        sb.Append("}\r\n");
+
     }
+    private void ProcessProduction(Production p, StringBuilder sb, bool first = false)
+    {
+        GenerateNestedProduction(p.Right, 0, sb, 1, first);
+    }
+    private void HandleEpsilone(bool has_eps,StringBuilder sb)
+    {
+        if(has_eps)
+        {
+            return;
+        }
+        int indent = 1;
+        sb.Append($"{Indentation(indent)}else\r\n");
+        sb.Append($"{Indentation(indent)}{{\r\n");
+        sb.Append($"{Indentation(indent + 1)}Unmatched();\r\n");
+        sb.Append($"{Indentation(indent)}}}\r\n");
+    }
+
     private void GenerateNestedProduction(List<Symbol> right, int pos,StringBuilder sb,int indent=0,bool first=false)
     {
         if (pos >= right.Count)
@@ -52,13 +76,22 @@ public class CSharpParserGnerator : ParserGenerator
         }
         if (cur.Kind == SymbolType.Terminal)
         {
-            sb.Append($"{Indentation(indent)}");
+            sb.Append($"\r\n{Indentation(indent)}");
             sb.Append(pos == 0 && !first ? "else if" : "if");
             sb.Append($"(Current()==\"{cur.Value}\")\r\n");
             sb.Append($"{Indentation(indent)}{{\r\n");
             sb.Append($"{Indentation(indent + 1)}Next();\r\n");
             GenerateNestedProduction(right, pos + 1, sb,indent+1);
             sb.Append($"{Indentation(indent)}}}\r\n");
+            if (pos == 0)
+                return;
+            sb.Append($"{Indentation(indent)}else\r\n");
+            sb.Append($"{Indentation(indent)}{{\r\n");
+            sb.Append($"{Indentation(indent+1)}Unmatched();\r\n");
+            sb.Append($"{Indentation(indent)}}}\r\n");
+
+
+
         }
         else
         {
